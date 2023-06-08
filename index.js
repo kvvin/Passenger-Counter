e381d68ca2ba56761551f7eae99f70901c15cdea
Kevin Thomas Manjooran <kevinthomastm2002@gmail.com>
2023-06-08 23:36:01 +0530

    completed code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-document.getElementById("count-el").innerText = 5;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increament()
+{
+    count += 1
+    countEl.textContent = count
+    
+}
+function save(){
+    let out = count + " - "
+    saveEl.textContent += out
+    countEl.textContent = 0
+    count = 0
+
+}
+
